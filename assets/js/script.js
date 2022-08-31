0d63031847077d7bf9558738c091044427e4294a
asmfpoly <panh9151@gmail.com>
2022-08-31 16:54:12 +0700

    theband

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+var slider = [
+    {
+        'heading': 'Los Angeles',
+        'description': 'We had the best time playing at Venice Beach!',
+        'url': '../assets/imgs/la.jpg'
+    },
+    {
+        'heading': 'New York',
+        'description': 'The atmosphere in New York is lorem ipsum.',
+        'url': '../assets/imgs/ny.jpg'
+    },
+    {
+        'heading': 'Chicago',
+        'description': 'Thank you, Chicago - A night we won\'t forget.',
+        'url': '../assets/imgs/chicago.jpg'
+    }
+]
+
+;(() => {
+    var index = 1
+    setInterval(() => {
+        document.querySelector('#slider').style.backgroundImage = `url(${slider[index].url})`
+        console.log(document.querySelector('#slider').style.backgroundImage);
+        document.querySelector('#slider .text-heading').textContent = slider[index].heading
+        document.querySelector('#slider .text-description').textContent = slider[index].description
+        index++
+        if (index > 2) index = 0
+    }, 3000)
+})()
